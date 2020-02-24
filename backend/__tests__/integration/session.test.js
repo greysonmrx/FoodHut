70c254c4b877f0182d64108e29ca32d28a52d64e
unknown <greysonmrx@gmail.com>
2020-02-24 08:36:26 -0300

    test(backend): add test to authenticate user

---
+++ backend/__tests__/integration/session.test.js
@@ -0,0 +1,24 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+import factory from '../factories';
+import Database from '../../src/database';
+import Admin from '../../src/app/models/Admin';
+
+describe('Authentication', () => {
+  beforeEach(async () => {
+    await Database.truncate();
+  });
+
+  it('should authenticate with valid credentials', async () => {
+    const { name, email, password } = await factory.attrs('Admin');
+
+    await Admin.create({ name, email, password });
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({ email, password });
+
+    expect(response.status).toBe(200);
+  });
+});
