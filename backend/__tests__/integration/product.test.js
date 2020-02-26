ea38fc21780e587570879b3690f3af6b2d15bc70
unknown <greysonmrx@gmail.com>
2020-02-26 10:28:58 -0300

    test(backend): add test to don't register a new product with duplicate name

--- backend/__tests__/integration/product.test.js
+++ backend/__tests__/integration/product.test.js
@@ -32,4 +32,31 @@ describe('Product', () => {
 
     expect(response.body).toHaveProperty('id');
   });
+
+  it('should not be able to register with duplicate name', async () => {
+    const product = await factory.attrs('Product');
+    const category = await factory.attrs('Category');
+    const admin = await factory.attrs('Admin');
+
+    const { id } = await Category.create(category);
+    const { email, password } = await Admin.create(admin);
+
+    const {
+      body: { token },
+    } = await request(app)
+      .post('/sessions')
+      .send({ email, password });
+
+    await request(app)
+      .post('/products')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ categories: [id], ...product });
+
+    const response = await request(app)
+      .post('/products')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ categories: [id], ...product });
+
+    expect(response.status).toBe(400);
+  });
 });
