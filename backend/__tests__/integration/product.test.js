dfcd07d045446a84d8844d0bc9c15164501bf2aa
unknown <greysonmrx@gmail.com>
2020-02-26 23:11:00 -0300

    test(backend): add test to update a product

--- backend/__tests__/integration/product.test.js
+++ backend/__tests__/integration/product.test.js
@@ -59,4 +59,33 @@ describe('Product', () => {
 
     expect(response.status).toBe(400);
   });
+
+  it('should be able to update', async () => {
+    const product = await factory.attrs('Product');
+    const category = await factory.attrs('Category');
+    const admin = await factory.attrs('Admin');
+
+    const { id } = await Category.create(category);
+    const { email, password } = await Admin.create(admin);
+
+    const {
+      body: { token },
+    } = await request(app)
+      .post('/sessions')
+      .send({ email, password });
+
+    const {
+      body: { id: productId },
+    } = await request(app)
+      .post('/products')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ categories: [id], ...product });
+
+    const response = await request(app)
+      .put(`/products/${productId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ categories: [id], ...product });
+
+    expect(response.body).toHaveProperty('id');
+  });
 });
