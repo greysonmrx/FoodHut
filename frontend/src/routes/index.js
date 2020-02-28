86a13d8926f4446b9eca8c55fc548adcbba280fe
unknown <greysonmrx@gmail.com>
2020-02-28 16:50:19 -0300

    feat(frontend): create signin page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 
+import SignIn from "../pages/SignIn";
+
 import Dashboard from "../pages/Dashboard";
 import Categories from "../pages/Categories";
 import Products from "../pages/Products";
@@ -9,6 +11,7 @@ import Users from "../pages/Users";
 export default function Routes() {
   return (
     <Switch>
+      <Route path="/" exact component={SignIn} />
       <Route path="/dashboard" exact component={Dashboard} />
       <Route path="/categories" exact component={Categories} />
       <Route path="/products" exact component={Products} />
