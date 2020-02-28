7aa02b93172bd6bbab84bef849b4900fe9655cb6
unknown <greysonmrx@gmail.com>
2020-02-28 16:51:39 -0300

    feat(frontend): create signup page

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 
 import SignIn from "../pages/SignIn";
+import SignUp from "../pages/SignUp";
 
 import Dashboard from "../pages/Dashboard";
 import Categories from "../pages/Categories";
@@ -12,6 +13,7 @@ export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/register" exact component={SignUp} />
       <Route path="/dashboard" exact component={Dashboard} />
       <Route path="/categories" exact component={Categories} />
       <Route path="/products" exact component={Products} />
