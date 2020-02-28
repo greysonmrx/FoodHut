efabff25bcfac29c33aa9df01e5eff340c961997
unknown <greysonmrx@gmail.com>
2020-02-28 17:49:23 -0300

    feat(frontend): use layouts on routes

--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -2,6 +2,9 @@ import React from "react";
 import PropTypes from "prop-types";
 import { Route, Redirect } from "react-router-dom";
 
+import AuthLayout from "../layouts/Auth";
+import DefaultLayout from "../layouts/Default";
+
 export default function RouteWrapper({
   component: Component,
   isPrivate = false,
@@ -17,7 +20,18 @@ export default function RouteWrapper({
     return <Redirect to="/dashboard" />;
   }
 
-  return <Route {...rest} component={Component} />;
+  const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return (
+    <Route
+      {...rest}
+      render={props => (
+        <Layout>
+          <Component {...props} />
+        </Layout>
+      )}
+    />
+  );
 }
 
 RouteWrapper.propTypes = {
