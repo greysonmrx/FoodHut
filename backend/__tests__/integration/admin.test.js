dcb29e18c0785f62e395ed77b2b983a24abb903d
unknown <greysonmrx@gmail.com>
2020-02-28 05:51:54 -0300

    test(backend): add test to register a new admin

---
+++ backend/__tests__/integration/admin.test.js
@@ -0,0 +1,31 @@
+import request from 'supertest';
+
+import app from '../../src/app';
+import factory from '../factories';
+import Database from '../../src/database';
+import Admin from '../../src/app/models/Admin';
+
+describe('Admin', () => {
+  beforeEach(async () => {
+    await Database.truncate();
+  });
+
+  it('should be able to register', async () => {
+    const admin = await factory.attrs('Admin');
+
+    const { email, password } = await Admin.create(admin);
+
+    const {
+      body: { token },
+    } = await request(app)
+      .post('/sessions')
+      .send({ email, password });
+
+    const response = await request(app)
+      .post('/admins')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'name', email: 'email@gmail.com', password: '123456' });
+
+    expect(response.body).toHaveProperty('id');
+  });
+});
