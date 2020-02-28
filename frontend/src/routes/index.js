abf5387d84a405c67a54e26390f7034cb4289bb9
unknown <greysonmrx@gmail.com>
2020-02-28 16:30:07 -0300

    feat(frontend): configure routes

---
+++ frontend/src/routes/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+import Dashboard from "../pages/Dashboard";
+import Categories from "../pages/Categories";
+import Products from "../pages/Products";
+import Users from "../pages/Users";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/dashboard" exact component={Dashboard} />
+      <Route path="/categories" exact component={Categories} />
+      <Route path="/products" exact component={Products} />
+      <Route path="/users" exact component={Users} />
+    </Switch>
+  );
+}
